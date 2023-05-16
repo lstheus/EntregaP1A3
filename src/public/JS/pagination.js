2618f1616b91b2c0def3a56957a9dd1b162c3a12
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-05-16 17:46:16 -0300

    Finalização - Paginação incompleta.

--- src/public/JS/pagination.js
+++ src/public/JS/pagination.js
@@ -5,9 +5,37 @@
 
 
 
+document.querySelector("#btn-entrar").addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/src/login/login.html";
+
+
+});
+
+document.querySelector("#btn-cadastrar").addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/src/register/register.html";
+
+
+});
+
+
+
+document.querySelector("#continue-btn").addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/src/myListsScreens/myListEmpty/index.html";
+
+
+});
+
+
+document.querySelector("#login-btn").addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/src/myListsScreens/myListEmpty/index.html";
+
+
+});
+// login-btn
+
 
 document.querySelector("#btn-back").addEventListener("click", () => {
- window.history.back()
+  window.history.back()
 
 });
 
@@ -15,30 +43,18 @@ document.querySelector("#btn-back").addEventListener("click", () => {
 
 
 document.querySelector("#logout").addEventListener("click", () => {
-  window.location.href="http://127.0.0.1:5500/src/TelaInicial/telaInicial.html";
+  window.location.href = "http://127.0.0.1:5500/src/TelaInicial/telaInicial.html";
 
- 
- });
+
+});
 
 //  <button class="btnEntrar" >Entre</button>
 
-            
+
 
 //  <span>Ou</span>
 
 
 // <button class="btnCadastrar">Cadastre-se</button></a>
 
-
-document.querySelector(".btnEntrar").addEventListener("click", () => {
-  window.location.href="http://127.0.0.1:5500/src/login/login.html";
-
- 
- });
-
- document.querySelector("#btnCadastrar").addEventListener("click", () => {
-  window.location.href="http://127.0.0.1:5500/src/register/register.html";
-
- 
- });
 
